0a86fdac2adff9f7af8097798b154d75d2646653
yomofo2s <yaqub_ajibola.omofoyewa@smail.th-koeln.de>
2020-12-09 23:28:58 +0000

    LEMP Docker config

---
+++ 19.03.13/.brew/docker.rb
@@ -0,0 +1,43 @@
+class Docker < Formula
+  desc "Pack, ship and run any application as a lightweight container"
+  homepage "https://www.docker.com/"
+  url "https://github.com/docker/docker-ce.git",
+      tag:      "v19.03.13",
+      revision: "4484c46d9d1a2d10b8fc662923ad586daeedb04f"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+  depends_on "go-md2man" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    dir = buildpath/"src/github.com/docker/cli"
+    dir.install (buildpath/"components/cli").children
+    cd dir do
+      commit = Utils.safe_popen_read("git", "rev-parse", "--short", "HEAD").chomp
+      build_time = Utils.safe_popen_read("date -u +'%Y-%m-%dT%H:%M:%SZ' 2> /dev/null").chomp
+      ldflags = ["-X \"github.com/docker/cli/cli/version.BuildTime=#{build_time}\"",
+                 "-X github.com/docker/cli/cli/version.GitCommit=#{commit}",
+                 "-X github.com/docker/cli/cli/version.Version=#{version}",
+                 "-X \"github.com/docker/cli/cli/version.PlatformName=Docker Engine - Community\""]
+      system "go", "build", "-o", bin/"docker", "-ldflags", ldflags.join(" "),
+             "github.com/docker/cli/cmd/docker"
+
+      Pathname.glob("man/*.[1-8].md") do |md|
+        section = md.to_s[/\.(\d+)\.md\Z/, 1]
+        (man/"man#{section}").mkpath
+        system "go-md2man", "-in=#{md}", "-out=#{man/"man#{section}"/md.stem}"
+      end
+
+      bash_completion.install "contrib/completion/bash/docker"
+      fish_completion.install "contrib/completion/fish/docker.fish"
+      zsh_completion.install "contrib/completion/zsh/_docker"
+    end
+  end
+
+  test do
+    assert_match "Docker version #{version}", shell_output("#{bin}/docker --version")
+    # Fails on CI: WARNING: No swap limit support
+    assert_match "ERROR: Cannot connect to the Docker daemon", shell_output("#{bin}/docker info", 1) if OS.mac?
+  end
+end
