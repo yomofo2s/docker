57b1cebe533081ebf1fe585f496b991cbbb8daa1
yomofo2s <yaqub_ajibola.omofoyewa@smail.th-koeln.de>
2020-12-10 00:37:14 +0000

    with php

---
+++ src/index.php
@@ -0,0 +1,18 @@
+!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello there</title>
+        <style>
+            .center {
+                display: block;
+                margin-left: auto;
+                margin-right: auto;
+                width: 50%;
+            }
+        </style>
+    </head>
+    <body>
+        <img src="https://iaenterprise.com/images/smilevoice/step3.jpg" alt="SMILE FOR ME" class="center">
+    </body>
+</html>
